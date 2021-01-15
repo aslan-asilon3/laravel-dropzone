0891129dfb702ed6cac66ec3c02105b9ab45cda2
shantanu0329 <shantanu.yadav@srmtechsol.com>
2021-01-16 01:57:46 +0530

    upload file dropzone completed

--- resources/views/dropzone.blade.php
+++ resources/views/dropzone.blade.php
@@ -5,6 +5,7 @@
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <meta name="csrf-token" content="{{ csrf_token() }}">
+
   <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
   <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
   <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
@@ -17,7 +18,7 @@
 <div class="container">
     <div class="jumbotron">
       <h2 class="text-center">Dropzone file upload</h2>
-        <form action="{{url('file-upload')}}" method="post" enctype="multipart/form-data" class="dropzone" id="kt_dropzone_2">
+        <form action="{{url('file-upload')}}" method="post" enctype="multipart/form-data" class="dropzone">
           @csrf
           <div class="fallback">
             <input name="file" type="file" multiple />
