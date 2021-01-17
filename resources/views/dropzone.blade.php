70fea49474264a1f443114f7d2c8d28fcd929936
shantanu0329 <shantanu.yadav@srmtechsol.com>
2021-01-17 23:42:06 +0530

    perfect dropzone

--- resources/views/dropzone.blade.php
+++ resources/views/dropzone.blade.php
@@ -10,7 +10,6 @@
   <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
   <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
   <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
-  <link rel="stylesheet" type="text/css" href="{{asset('dropzone/basic.min.css')}}">
   <link rel="stylesheet" type="text/css" href="{{asset('dropzone/dropzone.min.css')}}">
 </head>
 <body>
@@ -18,15 +17,51 @@
 <div class="container">
     <div class="jumbotron">
       <h2 class="text-center">Dropzone file upload</h2>
-        <form action="{{url('file-upload')}}" method="post" enctype="multipart/form-data" class="dropzone">
+        <form action="/file-upload" id="my-awesome-dropzone" class="dropzone">
           @csrf
-          <div class="fallback">
-            <input name="file" type="file" multiple />
-          </div>
         </form>
     </div>
+  <div id="success_box" class="alert alert-success alert-dismissable" style="display: none;"> <button type="button" class="close" data-dismiss="alert" aria-hidden="true">&times;</button><strong>Success !</strong>&emsp;All Files uploaded successfully.</div>    
 </div>
-
+<!-- https://developer.mozilla.org/en-US/docs/Web/HTTP/Basics_of_HTTP/MIME_types/Common_types -->
+<!-- https://www.dropzonejs.com -->
 <script type="text/javascript" src="{{asset('dropzone/dropzone.min.js')}}"></script>
+<script>
+      Dropzone.autoDiscover = false;
+      $("form#my-awesome-dropzone").dropzone({
+        url: "file-upload",
+        paramName: "file", // The name that will be used to transfer the file
+        maxFiles: 10,
+        uploadMultiple :false,
+        maxFilesize: 5, // MB
+        addRemoveLinks: true,
+        acceptedFiles: "image/*,application/pdf,application/msword",
+        accept: function(file, done) {
+            // console.log(file, done);
+            if (file.name == "justinbieber.jpg") {
+                done("Naha, you don't.");
+            } else {
+                done();
+            }
+        },
+        
+        init: function () {
+          this.on("complete", function (file) {
+            // console.log(file, done);
+            if(this.getUploadingFiles().length === 0 && this.getQueuedFiles().length === 0) {
+                if(file.status=="error"){
+                  console.log(file);
+                  return false;
+                }
+                $('#success_box').css('display','block');
+                setTimeout(function(){
+                  $('#success_box').css('display','none');
+                }, 4000);
+              }
+          });
+        }
+      });
+
+    </script>
 </body>
 </html>
